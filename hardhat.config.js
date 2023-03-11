7b49831e632006a698cf3e4b78dd24d8e22e2220
AnandSatwik <114983307+AnandSatwik@users.noreply.github.com>
2023-03-11 18:42:33 +0530

    Add files via upload

---
+++ hardhat.config.js
@@ -0,0 +1,17 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  networks:{
+    goerli:{
+      url:process.env.RPC_URL,
+      accounts:[process.env.USER] ,
+      
+    }
+  }
+};
+
